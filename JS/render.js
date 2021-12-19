aa2efcf84a105d011c5e3c3fc5c633b029b0fa93
Bambo0k <95869835+Bambo0k@users.noreply.github.com>
2021-12-19 08:57:27 +0200

    Fix letter space

--- JS/render.js
+++ JS/render.js
@@ -2,7 +2,7 @@ function render() {
     const region = document.getElementById('region').value;
     const pdensity = populationdensity();
     const text = `
-      В регионе ${region} плотность населения составит ${pdensity}человек на км².
+      В регионе ${region} плотность населения составит ${pdensity} человек на км².
       `;
     document.getElementById('result').innerHTML = text;
   }
