a967a4e833e11e7fffcd24258382f4d280dea864
Bambo0k <lisimas.g@gmail.com>
2021-12-22 05:07:27 +0200

    new div

--- JS/render.js
+++ JS/render.js
@@ -1,8 +1,16 @@
 function render() {
+    const population = document.getElementById('population').value;
+    const area = document.getElementById('area').value;
     const region = document.getElementById('region').value;
     const pdensity = populationdensity();
     const text = `
-      В регионе ${region} плотность населения составит ${pdensity} человек на км².
-      `;
-    document.getElementById('result').innerHTML = text;
+    В регионе ${region} плотность населения составит ${pdensity} человек на км².
+    `;
+    if (population && area) {
+      document.getElementById('banpopulation').innerHTML = "Введите данные!";
+    }
+  document.getElementById('result').innerHTML = text;
+  function populationdensity() {
+    return population / area ;
   }
+}
