c8758ab46dc3f2ba60929e15146576b3f0175e09
Bambo0k <95869835+Bambo0k@users.noreply.github.com>
2021-12-23 15:48:43 +0200

    Bug fixed and optimization

--- JS/culcpopulationdensity.js
+++ JS/culcpopulationdensity.js
@@ -1,6 +1,3 @@
-function populationdensity(x) {
-  const population = document.getElementById('population').value;
-  const area = document.getElementById('area').value;
-  return x = (population/area)
-;
-  }
+function populationdensity(a, b) {
+  return a/b ;
+}
--- JS/render.js
+++ JS/render.js
@@ -2,15 +2,15 @@ function render() {
     const population = document.getElementById('population').value;
     const area = document.getElementById('area').value;
     const region = document.getElementById('region').value;
-    const pdensity = populationdensity();
-    const text = `
-    В регионе ${region} плотность населения составит ${pdensity} человек на км².
-    `;
-    if (population && area) {
+    const pdensity = populationdensity(population, area);
+    if (!population) {
       document.getElementById('banpopulation').innerHTML = "Введите данные!";
     }
-  document.getElementById('result').innerHTML = text;
-  function populationdensity() {
-    return population / area ;
-  }
+    else if (!area) {
+      document.getElementById('banarea').innerHTML = "Введите данные!";
+    }
+      document.getElementById('result').innerHTML = text(region, pdensity);
+}
+function text(a, b) {  
+  return `В регионе ${a} плотность населения составит ${b} человек на км².`;
 }
