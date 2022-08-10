8aaae65784cebc2729675d38ae2c6d1c33ff2a14
Bambo0k <95869835+Bambo0k@users.noreply.github.com>
2022-08-11 00:39:31 +0300

    Linter

--- JS/culcpopulationdensity.js
+++ JS/culcpopulationdensity.js
@@ -1,3 +1,3 @@
 function populationdensity(a, b) {
-  return a/b ;
+    return a / b;
 }
--- JS/render.js
+++ JS/render.js
@@ -4,13 +4,13 @@ function render() {
     const region = document.getElementById('region').value;
     const pdensity = populationdensity(population, area);
     if (!population) {
-      document.getElementById('banpopulation').innerHTML = "Введите данные!";
+        document.getElementById('banpopulation').innerHTML = "Введите данные!";
+    } else if (!area) {
+        document.getElementById('banarea').innerHTML = "Введите данные!";
     }
-    else if (!area) {
-      document.getElementById('banarea').innerHTML = "Введите данные!";
-    }
-      document.getElementById('result').innerHTML = text(region, pdensity);
+    document.getElementById('result').innerHTML = text(region, pdensity);
 }
-function text(a, b) {  
-  return `В регионе ${a} плотность населения составит ${b} человек на км².`;
+
+function text(a, b) {
+    return `В регионе ${a} плотность населения составит ${b} человек на км².`;
 }
